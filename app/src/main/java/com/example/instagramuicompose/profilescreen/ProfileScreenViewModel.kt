6a62a05fa64026e84f2514ede1ecc726fd138e5b
Tadas <tadasgailevicius@yahoo.com>
2021-07-05 17:49:08 +0300

    Add ProfileScreenViewModel.kt ViewModel

---
+++ app/src/main/java/com/example/instagramuicompose/profilescreen/ProfileScreenViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.instagramuicompose.profilescreen
+
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ProfileScreenViewModel @Inject constructor(
+
+) : ViewModel() {
+}
